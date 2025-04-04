347ff4651e05b9ef2e9bb3556422109cd2bb1aeb
robin luiz <lerlfr3@gmail.com>
2025-04-04 16:24:14 +0200

    ex00-01

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ex00 / ex01',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'ex00 / ex01'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _text = 'Simple text';
+
+  void _incrementCounter() {
+    setState(() {
+      if (_text=='Simple text') {
+        _text = 'Hello World!';
+      } else {
+        _text = 'Simple text';
+      }
+    });
+    print('Button pressed');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+          Text(_text),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 13),
+              ),
+              child: const Text('Press me!'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
